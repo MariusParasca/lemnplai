625274bd2fe5496053e055890e541e6749c548ae
MariusParasca <mrs.parasca@gmail.com>
2019-08-18 10:42:48 +0300

    image slider dynamic

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,7 +1,45 @@
 import React from 'react';
 import './ImageSlider.css';
 
+// @ts-ignore
+const imageSlides = require.context(
+  '../../../public/assets/images/slides/',
+  false,
+  /^.*\.jpg$/
+);
+const BASE_IMAGE_NAME = 'slide';
+const IMAGE_EXTENSION = 'jpg';
+const NUMBER_OF_SLIDES = 3;
+
+const getCarouselImageTags = () => {
+  const imageTags = [];
+  imageTags.push(
+    <div className="carousel-item active">
+      <img
+        className="d-block img-responsive"
+        src={imageSlides(`./${BASE_IMAGE_NAME}1.${IMAGE_EXTENSION}`)}
+        alt={`${BASE_IMAGE_NAME}1`}
+      />
+    </div>
+  );
+  for (let slideNumber = 2; slideNumber <= NUMBER_OF_SLIDES; slideNumber++) {
+    imageTags.push(
+      <div className="carousel-item">
+        <img
+          className="d-block img-responsive"
+          src={imageSlides(
+            `./${BASE_IMAGE_NAME}${slideNumber}.${IMAGE_EXTENSION}`
+          )}
+          alt={`${BASE_IMAGE_NAME}${slideNumber}`}
+        />
+      </div>
+    );
+  }
+  return imageTags;
+};
+
 const imageSlider = () => {
+  const carouselImagesTags = getCarouselImageTags();
   return (
     <div
       id="myCarousel"
@@ -13,59 +51,25 @@ const imageSlider = () => {
         <li data-target="#myCarousel" data-slide-to="1"></li>
         <li data-target="#myCarousel" data-slide-to="2"></li>
       </ol>
-      <div
-        id="myCarouselControllers"
-        className="carousel slide"
-        data-ride="carousel"
+      <div className="carousel-inner">{carouselImagesTags}</div>
+      <a
+        className="carousel-control-prev"
+        href="#myCarousel"
+        role="button"
+        data-slide="prev"
       >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              className="d-block img-responsive"
-              src={require('../../assets/images/slides/slide1.jpg')}
-              alt="First slide"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              className="d-block img-responsive"
-              src={require('../../assets/images/slides/slide2.jpg')}
-              alt="Second slide"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              className="d-block img-responsive"
-              src={require('../../assets/images/slides/slide3.jpg')}
-              alt="Second slide"
-            />
-          </div>
-        </div>
-        <a
-          className="carousel-control-prev"
-          href="#myCarouselControllers"
-          role="button"
-          data-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#myCarouselControllers"
-          role="button"
-          data-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a
+        className="carousel-control-next"
+        href="#myCarousel"
+        role="button"
+        data-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
     </div>
   );
 };

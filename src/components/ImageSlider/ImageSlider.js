af5146b9a43a136cc18806dd66b51318ab1919f4
MariusParasca <mrs.parasca@gmail.com>
2019-08-18 19:19:51 +0300

    Added react awesome slider. Better

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+
 import './ImageSlider.css';
 import Spinner from '../UI/Spinner/Spinner';
 
@@ -13,112 +16,21 @@ const IMAGE_EXTENSION = 'jpg';
 const NUMBER_OF_SLIDES = 3;
 
 class ImageSlider extends Component {
-  state = {
-    loading: true
-  };
-
-  componentDidMount() {
-    this.setState({ loading: false });
-    console.log('componenetDidMount');
-  }
-
-  loadImages = () => {
-    const images = [];
-    images.push(imageSlides(`./${BASE_IMAGE_NAME}1.${IMAGE_EXTENSION}`));
-    for (let slideNumber = 2; slideNumber <= NUMBER_OF_SLIDES; slideNumber++) {
-      images.push(
-        imageSlides(`./${BASE_IMAGE_NAME}${slideNumber}.${IMAGE_EXTENSION}`)
-      );
-    }
-    return images;
-  };
-
-  getCarouselImageTags = imagesPaths => {
+  getCarouselImageTags = () => {
     const imageTags = [];
-    imageTags.push(
-      <div
-        className="carousel-item active"
-        key={`./${BASE_IMAGE_NAME}1.${IMAGE_EXTENSION}`}
-      >
-        <img
-          className="d-block img-responsive"
-          src={imagesPaths[0]}
-          alt={`${BASE_IMAGE_NAME}1`}
-        />
-      </div>
-    );
-    for (let slideNumber = 2; slideNumber <= NUMBER_OF_SLIDES; slideNumber++) {
+    for (let slideNumber = 1; slideNumber <= NUMBER_OF_SLIDES; slideNumber++) {
       const imageName = `./${BASE_IMAGE_NAME}${slideNumber}.${IMAGE_EXTENSION}`;
-      imageTags.push(
-        <div className="carousel-item" key={imageName}>
-          <img
-            className="d-block img-responsive"
-            src={imagesPaths[slideNumber - 1]}
-            alt={`${BASE_IMAGE_NAME}${slideNumber}`}
-          />
-        </div>
-      );
+      imageTags.push(<div data-src={imageSlides(imageName)} key={imageName} />);
     }
-
-    this.sleepFor(2000);
 
     return imageTags;
   };
 
-  sleepFor(sleepDuration) {
-    var now = new Date().getTime();
-    while (new Date().getTime() < now + sleepDuration) {
-      /* do nothing */
-    }
-  }
-
   render() {
-    let carouselImagesTags = [<Spinner key={'Spinner'} />];
-    if (!this.state.loading) {
-      carouselImagesTags = this.getCarouselImageTags(this.loadImages());
-    }
-
     return (
-      <div
-        id="myCarousel"
-        className="container-fluid carousel slide nopadding"
-        data-ride="carousel"
-      >
-        <ol className="carousel-indicators">
-          <li
-            data-target="#myCarousel"
-            data-slide-to="0"
-            className="active"
-          ></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner">{carouselImagesTags}</div>
-        <a
-          className="carousel-control-prev"
-          href="#myCarousel"
-          role="button"
-          data-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#myCarousel"
-          role="button"
-          data-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
+      <AwesomeSlider className=".aws-btn" infinite={true}>
+        {this.getCarouselImageTags()}
+      </AwesomeSlider>
     );
   }
 }

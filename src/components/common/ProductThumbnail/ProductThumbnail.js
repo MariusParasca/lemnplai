5e9802a6e0f9c5b50526f57fe2c421ec5ab1a3eb
MariusParasca <mrs.parasca@gmail.com>
2019-09-24 21:57:24 +0300

    Added lazy load on product images

--- src/components/common/ProductThumbnail/ProductThumbnail.js
+++ src/components/common/ProductThumbnail/ProductThumbnail.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react' 
+import LazyLoad from 'react-lazy-load';
 
 import classes from './ProductThumbnail.module.css';
 import Spinner from '../../UI/Spinner/Spinner';
@@ -10,13 +11,15 @@ const ProductThumbnail = (props) => {
   return (
     <div className={classes.Wrapper}>
       {loading ? <Spinner/> : null}
-      <img 
-        style={{display: !loading ? 'block' : 'none'}}
-        className={`${classes.Image} img-thumbnail`}
-        src={`${src}`}
-        onLoad={() => setLoading(false)}
-        alt={title}
-      />
+      <LazyLoad>
+        <img 
+          style={{display: !loading ? 'block' : 'none'}}
+          className={`${classes.Image} img-thumbnail`}
+          src={`${src}`}
+          onLoad={() => setLoading(false)}
+          alt={title}
+        />
+      </LazyLoad>
       <div className="caption">
         <h3>{title}</h3>
         <p className={classes.Description}>{description}</p>
